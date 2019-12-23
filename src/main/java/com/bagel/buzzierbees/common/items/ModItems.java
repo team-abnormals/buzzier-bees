80c62ac27637c9343eb83aec557ecfdf6ac21255
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2019-12-23 12:07:18 -0600

    Removed Clover Honey Bottle from Master

--- src/main/java/com/bagel/buzzierbees/common/items/ModItems.java
+++ src/main/java/com/bagel/buzzierbees/common/items/ModItems.java
@@ -1,7 +1,6 @@
 package com.bagel.buzzierbees.common.items;
 
 import net.minecraft.item.Food;
-import net.minecraft.item.HoneyBottleItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.potion.EffectInstance;
@@ -17,16 +16,13 @@ public class ModItems
 	public static Item WAX;
 	public static Item CRYSTALLIZED_HONEY;
 	public static Item BEE_SOUP;
-	public static Item CLOVER_HONEY_BOTTLE;
 
 	public static Food CRYSTALLIZED_HONEY_FOOD;
 	public static Food BEE_SOUP_FOOD;
-	public static Food CLOVER_HONEY_BOTTLE_FOOD;
 	
 	static {
 		CRYSTALLIZED_HONEY_FOOD = (new Food.Builder()).hunger(1).saturation(1.5F).fastToEat().setAlwaysEdible().effect(new EffectInstance(Effects.SPEED, 160, 1), 0.8F).build();
 		BEE_SOUP_FOOD = (new Food.Builder()).hunger(8).saturation(0.6F).setAlwaysEdible().effect(new EffectInstance(Effects.SLOWNESS, 240, 2), 0.5F).build();
-		CLOVER_HONEY_BOTTLE_FOOD = (new Food.Builder()).hunger(6).saturation(2.4F).setAlwaysEdible().effect(new EffectInstance(Effects.INSTANT_HEALTH, 20, 1), 1F).build();
 
 	}
 	
@@ -36,8 +32,6 @@ public class ModItems
     	CRYSTALLIZED_HONEY = registerItem(new Item(new Item.Properties().food(CRYSTALLIZED_HONEY_FOOD).group(ItemGroup.FOOD)), "crystallized_honey");
         WAX = registerItem(new Item(new Item.Properties().group(ItemGroup.MATERIALS)), "wax");
     	BEE_SOUP = registerItem(new BeeSoupItem(new Item.Properties().maxStackSize(1).food(BEE_SOUP_FOOD).group(ItemGroup.FOOD)), "bee_soup");
-    	CLOVER_HONEY_BOTTLE = registerItem(new HoneyBottleItem(new Item.Properties().maxStackSize(16).food(CLOVER_HONEY_BOTTLE_FOOD).group(ItemGroup.FOOD)), "clover_honey_bottle");
-
 	}
 	
     public static Item registerItem(Item item, String name)
