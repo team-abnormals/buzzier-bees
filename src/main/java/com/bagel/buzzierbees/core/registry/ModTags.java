11d1244fd19c24a7f92e258ead55b45f0b707ecc
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2020-02-27 16:34:31 -0600

    Fixed an issue with falling candles

---
+++ src/main/java/com/bagel/buzzierbees/core/registry/ModTags.java
@@ -0,0 +1,15 @@
+package com.bagel.buzzierbees.core.registry;
+
+
+import com.bagel.buzzierbees.core.BuzzierBees;
+
+import net.minecraft.item.Item;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.Tag;
+import net.minecraft.util.ResourceLocation;
+
+public class ModTags {
+	public static final Tag<Item> CANDLES = new ItemTags.Wrapper(new ResourceLocation(BuzzierBees.MODID, "candles"));
+	public static final Tag<Item> DYED_CANDLES = new ItemTags.Wrapper(new ResourceLocation(BuzzierBees.MODID, "dyed_candles"));
+	public static final Tag<Item> SCENTED_CANDLES = new ItemTags.Wrapper(new ResourceLocation(BuzzierBees.MODID, "scented_candles"));
+}
