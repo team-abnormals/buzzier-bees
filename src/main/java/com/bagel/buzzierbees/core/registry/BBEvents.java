914d009638063dac27c5036c68ce9ff8f00d659a
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2020-03-20 20:29:27 -0500

    Fixed bottle duplication ( again )

--- src/main/java/com/bagel/buzzierbees/core/registry/BBEvents.java
+++ src/main/java/com/bagel/buzzierbees/core/registry/BBEvents.java
@@ -83,7 +83,7 @@ public class BBEvents {
     			bottleItem = bottleItem.setDisplayName(name);
     		}
     		
-			if(successful && ((MobEntity) target).getHealth() > 0) {
+			if(successful && ((MobEntity) target).isAlive()) {
 				if(item == Items.GLASS_BOTTLE) {
 					itemstack.shrink(1);
 					event.getWorld().playSound(player, event.getPos(), SoundEvents.ITEM_BOTTLE_FILL, SoundCategory.BLOCKS, 1.0F, 1.0F);
