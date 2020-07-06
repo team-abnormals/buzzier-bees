28b14349b5c875411043408d642f1e10a07dd967
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2020-07-05 22:26:13 -0500

    Hello more tiny fixes!

--- src/main/java/com/bagel/buzzierbees/core/other/BBFoods.java
+++ src/main/java/com/bagel/buzzierbees/core/other/BBFoods.java
@@ -4,13 +4,12 @@ import net.minecraft.item.Food;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.Effects;
 
-@SuppressWarnings("deprecation")
 public class BBFoods {
-	public static Food CRYSTALLIZED_HONEY  = new Food.Builder().hunger(1).saturation(1.5F).fastToEat().setAlwaysEdible().effect(new EffectInstance(Effects.SPEED, 160, 1), 0.8F).build();
-	public static Food BEE_SOUP            = new Food.Builder().hunger(3).saturation(2.3F).effect(new EffectInstance(Effects.SLOWNESS, 240, 2), 0.5F).build();
-	public static Food CLOVER_HONEY_BOTTLE = new Food.Builder().hunger(6).saturation(0.2F).setAlwaysEdible().effect(new EffectInstance(Effects.INSTANT_HEALTH, 20, 1), 0.8F).build();
-	public static Food STICKY_HONEY_WAND   = new Food.Builder().hunger(6).saturation(0.1F).setAlwaysEdible().build();
-	public static Food HONEY_APPLE         = new Food.Builder().hunger(5).saturation(0.4F).build();
-	public static Food HONEY_BREAD         = new Food.Builder().hunger(6).saturation(0.8F).build();
-	public static Food GLAZED_PORKCHOP	   = new Food.Builder().hunger(9).saturation(0.8F).build();
+	public static final Food CRYSTALLIZED_HONEY  = new Food.Builder().hunger(1).saturation(1.5F).fastToEat().setAlwaysEdible().effect(() -> new EffectInstance(Effects.SPEED, 160, 1), 0.8F).build();
+	public static final Food BEE_SOUP            = new Food.Builder().hunger(3).saturation(2.3F).effect(() -> new EffectInstance(Effects.SLOWNESS, 240, 2), 0.5F).build();
+	public static final Food CLOVER_HONEY_BOTTLE = new Food.Builder().hunger(6).saturation(0.2F).setAlwaysEdible().effect(() -> new EffectInstance(Effects.INSTANT_HEALTH, 20, 1), 0.8F).build();
+	public static final Food STICKY_HONEY_WAND   = new Food.Builder().hunger(6).saturation(0.1F).setAlwaysEdible().build();
+	public static final Food HONEY_APPLE         = new Food.Builder().hunger(5).saturation(0.4F).build();
+	public static final Food HONEY_BREAD         = new Food.Builder().hunger(6).saturation(0.8F).build();
+	public static final Food GLAZED_PORKCHOP		   = new Food.Builder().hunger(9).saturation(0.8F).build();
 }
