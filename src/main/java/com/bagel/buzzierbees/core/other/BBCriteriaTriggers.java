6bd389b545315185abb5a271d8bfa57af9d2b7d9
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2020-07-05 21:54:54 -0500

    Goodbye misc. stuff.

--- src/main/java/com/bagel/buzzierbees/core/other/BBCriteriaTriggers.java
+++ src/main/java/com/bagel/buzzierbees/core/other/BBCriteriaTriggers.java
@@ -12,7 +12,6 @@ public class BBCriteriaTriggers {
 	public static final EmptyTrigger HONEY_APPLE_CURE = CriteriaTriggers.register(new EmptyTrigger(prefix("honey_apple_cure")));
 	public static final EmptyTrigger HONEY_BREAD_CURE = CriteriaTriggers.register(new EmptyTrigger(prefix("honey_bread_cure")));
 	public static final EmptyTrigger GLAZED_PORKCHOP_CURE = CriteriaTriggers.register(new EmptyTrigger(prefix("glazed_porkchop_cure")));
-	public static final EmptyTrigger USE_CURE = CriteriaTriggers.register(new EmptyTrigger(prefix("use_cure")));
 	
 	private static ResourceLocation prefix(String name) {
 		return new ResourceLocation(BuzzierBees.MODID, name);
--- src/main/java/com/bagel/buzzierbees/core/other/BBTags.java
+++ src/main/java/com/bagel/buzzierbees/core/other/BBTags.java
@@ -4,18 +4,12 @@ package com.bagel.buzzierbees.core.other;
 import com.bagel.buzzierbees.core.BuzzierBees;
 
 import net.minecraft.block.Block;
-import net.minecraft.item.Item;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.tags.ITag.INamedTag;
-import net.minecraft.tags.ItemTags;
 
 public class BBTags {
 	
 	static class Blocks {
 		public static final INamedTag<Block> FLOWER_BLACKLIST = BlockTags.makeWrapperTag(BuzzierBees.MODID + ":flower_blacklist");
-	}
-
-	static class Items {
-		public static final INamedTag<Item> MODDED_POTTABLES = ItemTags.makeWrapperTag(BuzzierBees.MODID + ":modded_pottables");
 	}
 }
