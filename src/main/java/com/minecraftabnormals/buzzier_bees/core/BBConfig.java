3cb64b9b9db86adb193854f72fd176b6df25fc32
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2020-09-13 23:25:40 -0500

    Configuration for Flower renewability

---
+++ src/main/java/com/minecraftabnormals/buzzier_bees/core/BBConfig.java
@@ -0,0 +1,32 @@
+package com.minecraftabnormals.buzzier_bees.core;
+
+import org.apache.commons.lang3.tuple.Pair;
+
+import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.common.ForgeConfigSpec.ConfigValue;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+
+@EventBusSubscriber(modid = BuzzierBees.MODID)
+public class BBConfig {
+
+    public static class Common {
+        public final ConfigValue<Boolean> tallFlowerDuplication;
+        public final ConfigValue<Boolean> shortFlowerDuplication;
+
+        Common(ForgeConfigSpec.Builder builder) {
+            builder.push("flowers");
+            tallFlowerDuplication = builder.define("Allow bonemealing tall flowers to duplicate them", true);
+            shortFlowerDuplication = builder.define("Allow bonemealing short flowers to duplicate them", true);
+            builder.pop();
+        }
+    }
+    
+    public static final ForgeConfigSpec COMMON_SPEC;
+    public static final Common COMMON;
+
+    static {
+        final Pair<Common, ForgeConfigSpec> specPair = new ForgeConfigSpec.Builder().configure(Common::new);
+        COMMON_SPEC = specPair.getRight();
+        COMMON = specPair.getLeft();
+    }
+}
