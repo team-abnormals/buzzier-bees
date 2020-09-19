00114cc82123083edc5cb7828648a2c84b9d5637
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2020-09-19 13:35:25 -0500

    Mixin to fix Beehives rotation in Structures

---
+++ src/main/java/com/minecraftabnormals/buzzier_bees/core/mixin/BeehiveBlockMixin.java
@@ -0,0 +1,28 @@
+package com.minecraftabnormals.buzzier_bees.core.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+
+import net.minecraft.block.BeehiveBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.util.Mirror;
+import net.minecraft.util.Rotation;
+
+@Mixin(BeehiveBlock.class)
+public class BeehiveBlockMixin extends Block{
+	
+	public BeehiveBlockMixin(Properties properties) {
+		super(properties);
+	}
+
+	@Override
+	public BlockState rotate(BlockState state, Rotation rot) {
+		return state.with(BeehiveBlock.FACING, rot.rotate(state.get(BeehiveBlock.FACING)));
+	}
+
+	@SuppressWarnings("deprecation")
+	@Override
+	public BlockState mirror(BlockState state, Mirror mirrorIn) {
+		return mirrorIn == Mirror.NONE ? state : state.rotate(mirrorIn.toRotation(state.get(BeehiveBlock.FACING)));
+	}
+}
