e5ff95cffb57537883d9dc02c33572d22f4dcae6
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2020-11-24 01:31:03 -0600

    Increased Buttercup generation rate

--- src/main/java/com/minecraftabnormals/buzzier_bees/core/registry/BBFeatures.java
+++ src/main/java/com/minecraftabnormals/buzzier_bees/core/registry/BBFeatures.java
@@ -32,7 +32,7 @@ public class BBFeatures {
             if (biome == Biomes.FLOWER_FOREST || biome == Biomes.BIRCH_FOREST || biome == Biomes.BIRCH_FOREST_HILLS || biome == Biomes.TALL_BIRCH_FOREST || biome == Biomes.TALL_BIRCH_HILLS)
             	addShortFlower(BBBlocks.PINK_CLOVER.get().getDefaultState(), biome, 3);
             if (biome == Biomes.FLOWER_FOREST || biome == Biomes.SUNFLOWER_PLAINS)
-            	addShortFlower(BBBlocks.BUTTERCUP.get().getDefaultState(), biome, 2);
+            	addShortFlower(BBBlocks.BUTTERCUP.get().getDefaultState(), biome, 4);
         }
     }
 }
