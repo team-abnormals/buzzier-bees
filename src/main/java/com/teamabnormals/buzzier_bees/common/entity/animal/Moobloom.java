ddbc5982fe96a07b89d6ca5e56ac63e0349a5f70
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2025-09-03 18:05:46 -0500

    Closes #208

--- src/main/java/com/teamabnormals/buzzier_bees/common/entity/animal/Moobloom.java
+++ src/main/java/com/teamabnormals/buzzier_bees/common/entity/animal/Moobloom.java
@@ -4,10 +4,13 @@ import com.teamabnormals.buzzier_bees.core.registry.BBBlocks;
 import com.teamabnormals.buzzier_bees.core.registry.BBEntityTypes;
 import com.teamabnormals.buzzier_bees.core.registry.BBMobEffects;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.component.DataComponents;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.AgeableMob;
 import net.minecraft.world.entity.EntityType;
@@ -17,14 +20,19 @@ import net.minecraft.world.entity.animal.Cow;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.ItemUtils;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.component.SuspiciousStewEffects;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SuspiciousEffectHolder;
 import net.minecraft.world.level.gameevent.GameEvent;
 
 import javax.annotation.Nullable;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 public class Moobloom extends Cow implements Shearable {
 
@@ -60,6 +68,25 @@ public class Moobloom extends Cow implements Shearable {
 					living.addEffect(new MobEffectInstance(BBMobEffects.SUNNY, 100, 0, false, false));
 			}
 		}
+	}
+
+	@Override
+	public InteractionResult mobInteract(Player player, InteractionHand hand) {
+		ItemStack stack = player.getItemInHand(hand);
+		if (stack.is(Items.BOWL) && !this.isBaby()) {
+			ItemStack stewStack = new ItemStack(Items.SUSPICIOUS_STEW);
+			stewStack.set(DataComponents.SUSPICIOUS_STEW_EFFECTS, this.getEffectsFromItemStack(new ItemStack(BBBlocks.BUTTERCUP)).get());
+			player.setItemInHand(hand, ItemUtils.createFilledResult(stack, player, stewStack, false));
+			this.playSound(SoundEvents.MOOSHROOM_MILK_SUSPICIOUSLY, 1.0F, 1.0F);
+			return InteractionResult.sidedSuccess(this.level().isClientSide());
+		} else {
+			return super.mobInteract(player, hand);
+		}
+	}
+
+	private Optional<SuspiciousStewEffects> getEffectsFromItemStack(ItemStack stack) {
+		SuspiciousEffectHolder effect = SuspiciousEffectHolder.tryGet(stack.getItem());
+		return effect != null ? Optional.of(effect.getSuspiciousEffects()) : Optional.empty();
 	}
 
 	@Override
