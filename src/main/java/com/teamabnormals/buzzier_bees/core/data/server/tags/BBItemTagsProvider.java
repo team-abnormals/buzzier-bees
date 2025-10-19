a86c5ad50def7a397ddfe9b28cd7bc06cfe2cba0
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2025-10-18 23:38:23 -0500

    Tag tweaks

--- src/main/java/com/teamabnormals/buzzier_bees/core/data/server/tags/BBItemTagsProvider.java
+++ src/main/java/com/teamabnormals/buzzier_bees/core/data/server/tags/BBItemTagsProvider.java
@@ -32,15 +32,17 @@ public class BBItemTagsProvider extends ItemTagsProvider {
 		this.copy(BlockTags.STAIRS, ItemTags.STAIRS);
 		this.copy(BlockTags.WALLS, ItemTags.WALLS);
 		this.copy(BlockTags.CANDLES, ItemTags.CANDLES);
-		this.tag(ItemTags.PIGLIN_REPELLENTS).add(BBBlocks.SOUL_CANDLE.get().asItem());
+		this.tag(ItemTags.PIGLIN_REPELLENTS).add(BBBlocks.SOUL_CANDLE.asItem());
 
 		this.copy(BlockTags.DOORS, ItemTags.DOORS);
 		this.copy(BlockTags.TRAPDOORS, ItemTags.TRAPDOORS);
 
 		this.tag(BBItemTags.GRIZZLY_BEAR_FOOD).add(Items.SALMON, Items.COOKED_SALMON, Items.SALMON_BUCKET);
 
+		this.tag(ItemTags.MEAT).add(GLAZED_PORKCHOP.get());
+		this.tag(Tags.Items.FOODS).add(GLAZED_PORKCHOP.get()).addTag(BBItemTags.FOODS_PASTRY);
 		this.tag(Tags.Items.FOODS_FRUIT).add(HONEY_APPLE.get());
-		this.tag(Tags.Items.FOODS_BREAD).add(HONEY_BREAD.get());
-		this.tag(Tags.Items.FOODS_COOKED_MEAT).add(GLAZED_PORKCHOP.get());
+		this.tag(BBItemTags.FOODS_PASTRY).add(HONEY_BREAD.get());
+		this.tag(Tags.Items.ANIMAL_FOODS).addTag(BBItemTags.GRIZZLY_BEAR_FOOD);
 	}
 }
--- src/main/java/com/teamabnormals/buzzier_bees/core/other/tags/BBItemTags.java
+++ src/main/java/com/teamabnormals/buzzier_bees/core/other/tags/BBItemTags.java
@@ -5,16 +5,15 @@ import com.teamabnormals.buzzier_bees.core.BuzzierBees;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
 
+import javax.swing.text.html.HTML.Tag;
+
 public class BBItemTags {
+	public static final TagKey<Item> FOODS_PASTRY = TagUtil.itemTag("c", "foods/pastry");
 	public static final TagKey<Item> GRIZZLY_BEAR_FOOD = itemTag("grizzly_bear_food");
-	public static final TagKey<Item> ENDER_FIRE_BASE_BLOCKS = itemTag("endergetic", "ender_fire_base_blocks");
-	public static final TagKey<Item> CUPRIC_FIRE_BASE_BLOCKS = itemTag("caverns_and_chasms", "cupric_fire_base_blocks");
+	public static final TagKey<Item> ENDER_FIRE_BASE_BLOCKS = TagUtil.itemTag("endergetic", "ender_fire_base_blocks");
+	public static final TagKey<Item> CUPRIC_FIRE_BASE_BLOCKS = TagUtil.itemTag("caverns_and_chasms", "cupric_fire_base_blocks");
 
 	private static TagKey<Item> itemTag(String name) {
 		return TagUtil.itemTag(BuzzierBees.MOD_ID, name);
-	}
-
-	private static TagKey<Item> itemTag(String mod, String name) {
-		return TagUtil.itemTag(mod, name);
 	}
 }
