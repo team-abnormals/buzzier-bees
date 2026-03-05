c52757b6c42ea53071eb15e3779a327cf8ef3f31
Mohammad Merghani <114697909+krypt0graphy@users.noreply.github.com>
2026-03-06 03:07:22 +0400

    Closes #213

--- src/main/java/com/teamabnormals/buzzier_bees/core/other/BBEvents.java
+++ src/main/java/com/teamabnormals/buzzier_bees/core/other/BBEvents.java
@@ -11,6 +11,7 @@ import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
 import net.minecraft.world.entity.monster.Phantom;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
@@ -40,12 +41,17 @@ public class BBEvents {
 	public static void renewableFlowers(BonemealEvent event) {
 		Level level = event.getLevel();
 		BlockPos pos = event.getPos();
+		ItemStack stack = event.getStack();
+		Player player = event.getPlayer();
 		BlockState state = level.getBlockState(pos);
 		Block block = state.getBlock();
 
 		if (BBConfig.COMMON.shortFlowerDuplication.get() && block instanceof FlowerBlock && !state.hasBlockEntity() && !state.is(BBBlockTags.FLOWER_BLACKLIST) && !(block instanceof BonemealableBlock bonemealableBlock && bonemealableBlock.isBonemealSuccess(level, level.random, pos, level.getBlockState(pos)))) {
 			if (!level.isClientSide()) {
 				Block.popResource(level, pos, new ItemStack(block));
+			}
+			if (player != null && !player.isCreative()) {
+				stack.shrink(1);
 			}
 			event.setSuccessful(true);
 		}
